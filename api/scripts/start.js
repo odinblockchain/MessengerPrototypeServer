49ff24b534e64077415b36a85ab64598a835f508
Manbearpixel <manbearpixel@gmail.com>
2018-04-12 22:42:28 -0500

    Update to API for local development

--- api/scripts/start.js
+++ api/scripts/start.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { exec }  = require('child_process');
-const Port      = process.env.PORT || 4200;
+const Port      = process.env.PORT || 4201;
 const NodeEnv   = process.env.NODE_ENV || 'development';
 
 console.log(`:// MessengerPrototypeServer [Starting] ... Listening on :${Port}`);
